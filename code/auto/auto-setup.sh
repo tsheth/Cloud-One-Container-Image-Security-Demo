24b9a003079b1fa5dba057d715d45c5dd6bf210f
Will Robinson <will@oznetnerd.com>
2019-08-06 09:52:37 +1000

    multiple updates

--- code/auto/auto-setup.sh
+++ code/auto/auto-setup.sh
@@ -4,15 +4,15 @@ sed -e s/"sudo "//g -i docker-setup.sh
 chmod +x docker-setup.sh
 ./docker-setup.sh
 rm docker-setup.sh
-wget https://raw.githubusercontent.com/OzNetNerd/Deep-Security-Smart-Check-Demo/master/code/manual/kube-setup.sh
+wget https://raw.githubusercontent.com/OzNetNerd/Deep-Security-Smart-Check-Demo/master/code/scripts/kube-setup.sh
 chmod +x kube-setup.sh
 ./kube-setup.sh
 rm kube-setup.sh
 mkdir -p /home/ec2-user/.kube
 eksctl create cluster --name="$1" --nodes=3 --region="$2" --kubeconfig=/home/ec2-user/.kube/config
 wget https://raw.githubusercontent.com/OzNetNerd/Deep-Security-Smart-Check-Demo/master/code/scripts/Makefile
-mkdir /home/ec2-user/kubernetes-config
+mkdir -p /home/ec2-user/kubernetes-config
 mv ./Makefile /home/ec2-user/kubernetes-config
 cd /home/ec2-user/kubernetes-config
-make start AWS_REGION="$2"
+make "$3" AWS_REGION="$2"
 chown -R ec2-user:ec2-user /home/ec2-user
