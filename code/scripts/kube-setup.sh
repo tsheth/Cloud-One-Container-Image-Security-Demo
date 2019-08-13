5321a4107abe0851d5e6fcbe8ce7ab39341a5b2d
Will Robinson <will@oznetnerd.com>
2019-08-13 15:38:15 +1000

    update

--- code/scripts/kube-setup.sh
+++ code/scripts/kube-setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/latest_release/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 chmod +x /tmp/eksctl
-mv /tmp/eksctl /usr/local/bin
+sudo mv /tmp/eksctl /usr/local/bin
 curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.13.7/2019-06-11/bin/linux/amd64/aws-iam-authenticator
 chmod +x ./aws-iam-authenticator
 sudo mv aws-iam-authenticator /usr/local/bin
