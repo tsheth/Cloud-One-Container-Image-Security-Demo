cd618ec94db6e0ced63b309cfffc70f9df7afb31
Will Robinson <will@oznetnerd.com>
2019-08-13 15:25:35 +1000

    updated script

--- code/scripts/kube-setup.sh
+++ code/scripts/kube-setup.sh
@@ -4,10 +4,10 @@ chmod +x /tmp/eksctl
 mv /tmp/eksctl /usr/local/bin
 curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.13.7/2019-06-11/bin/linux/amd64/aws-iam-authenticator
 chmod +x ./aws-iam-authenticator
-mv aws-iam-authenticator /usr/local/bin
+sudo mv aws-iam-authenticator /usr/local/bin
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
-mv ./kubectl /usr/local/bin/kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
 yum install bash-completion --enablerepo=epel -y
 mkdir -p /home/ec2-user
 echo 'source <(kubectl completion bash)' >>/home/ec2-user/.bashrc
@@ -15,4 +15,5 @@ curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_
 chmod +x get_helm.sh
 ./get_helm.sh
 rm -rf ./get_helm.sh
+helm init
 chown -R ec2-user:ec2-user /home/ec2-user
